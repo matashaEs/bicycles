88c25f1b0e344f477201973d651823279c3cd15d
matashaEs <natali.babinska@ukr.net>
2024-05-23 15:57:42 +0200

    delete auth

--- get_auth_token.php
+++
@@ -1,5 +0,0 @@
-<?php
-$redirectUri = 'http://localhost/project/get_auth_token.php';
-$clientId = '785116429270-n3svfmpadgsc7v7qi1d7bt9j8ieral42.apps.googleusercontent.com';
-$clientSecret = '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';
-?>
